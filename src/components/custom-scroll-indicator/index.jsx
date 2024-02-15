81205a3316d8382d1fce39f6edbb068ef642feec
anyars-encarta <anyarsencarta@gmail.com>
2024-02-15 08:56:50 +0000

    Initialise Custom Scroll Indicator component

---
+++ src/components/custom-scroll-indicator/index.jsx
@@ -0,0 +1,9 @@
+const CustomScrollIndicator = () => {
+    return (
+        <div>
+            This is my Custom Scroll Component
+        </div>
+    )
+};
+
+export default CustomScrollIndicator;
